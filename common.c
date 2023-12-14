abde402706d8b59cb27f581bb8a5d09f2fbea979
Attractive Chaos <attractor@live.co.uk>
2023-12-14 11:07:18 -0500

    initial version

--- common.c
+++ common.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 
@@ -9,14 +10,20 @@
  * Measuring CPU time and peak RSS *
  ***********************************/
 
-static double cputime(void)
+typedef struct {
+	uint32_t n_input, table_size;
+	uint64_t checksum;
+	double t, mem;
+} udb_checkpoint_t;
+
+static double udb_cputime(void)
 {
 	struct rusage r;
 	getrusage(RUSAGE_SELF, &r);
 	return r.ru_utime.tv_sec + r.ru_stime.tv_sec + 1e-6 * (r.ru_utime.tv_usec + r.ru_stime.tv_usec);
 }
 
-static long peakrss(void)
+static long udb_peakrss(void)
 {
 	struct rusage r;
 	getrusage(RUSAGE_SELF, &r);
@@ -27,11 +34,20 @@ static long peakrss(void)
 #endif
 }
 
+static void udb_measure(uint32_t n_input, uint32_t table_size, uint64_t checksum, udb_checkpoint_t *cp)
+{
+	cp->t = udb_cputime();
+	cp->mem = udb_peakrss();
+	cp->n_input = n_input;
+	cp->table_size = table_size;
+	cp->checksum = checksum;
+}
+
 /******************
  * Key generation *
  ******************/
 
-static inline uint32_t hash32(uint32_t key)
+static inline uint32_t udb_hash32(uint32_t key)
 {
     key += ~(key << 15);
     key ^=  (key >> 10);
@@ -42,22 +58,22 @@ static inline uint32_t hash32(uint32_t key)
     return key;
 }
 
-static inline uint32_t get_key(const uint32_t n, const uint32_t x)
+static inline uint32_t udb_get_key(const uint32_t n, const uint32_t x)
 {
-	return hash32(x % (n>>2));
+	return udb_hash32(x % (n>>2));
 }
 
 /**********************************************
  * For testing key generation time (baseline) *
  **********************************************/
 
-uint64_t traverse_rng(uint32_t n, uint32_t x0)
+uint64_t udb_traverse_rng(uint32_t n, uint32_t x0)
 {
 	uint64_t sum = 0;
 	uint32_t i, x;
 	for (i = 0, x = x0; i < n; ++i) {
-		x = hash32(x);
-		sum += get_key(n, x);
+		x = udb_hash32(x);
+		sum += udb_get_key(n, x);
 	}
 	return sum;
 }
@@ -66,7 +82,7 @@ uint64_t traverse_rng(uint32_t n, uint32_t x0)
  * This is the hash function used by almost everyone *
  *****************************************************/
 
-static inline uint32_t hash_fn(uint32_t key)
+static inline uint32_t udb_hash_fn(uint32_t key)
 {
 	return key;
 }
@@ -75,37 +91,48 @@ static inline uint32_t hash_fn(uint32_t key)
  * Main function *
  *****************/
 
+void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_cp, udb_checkpoint_t *cp);
+
 int main(int argc, char *argv[])
 {
-	uint32_t test_int(uint32_t n, uint32_t x0);
 	int c;
-	double t, t0;
-	uint32_t i, m = 5, max = 50000000, n = 10000000, x0 = 1, step;
+	double t0, t_keygen;
 	uint64_t sum;
-	long m0;
+	uint32_t i, n_cp = 6, N = 60000000, n0 = 10000000, x0 = 1, is_del = 0;
+	udb_checkpoint_t cp0, *cp;
 
-	while ((c = getopt(argc, argv, "n:x:0:k:")) >= 0) {
-		if (c == 'n') n = atol(optarg);
-		else if (c == 'x') max = atol(optarg);
+	while ((c = getopt(argc, argv, "n:N:0:k:d")) >= 0) {
+		if (c == 'n') n0 = atol(optarg);
+		else if (c == 'N') N = atol(optarg);
 		else if (c == '0') x0 = atol(optarg);
-		else if (c == 'k') m = atoi(optarg);
+		else if (c == 'k') n_cp = atoi(optarg);
+		else if (c == 'd') is_del = 1;
 	}
 
-	t = cputime();
-	sum = traverse_rng(n, x0);
-	t0 = cputime() - t;
-	fprintf(stderr, "CPU time spent on RNG: %.3f sec; total sum: %lu\n", t0, (unsigned long)sum);
-	m0 = peakrss();
+	printf("CL\tUsage: run-test [options]\n");
+	printf("CL\tOptions:\n");
+	printf("CL\t  -d         evaluate insertion/deletion (insertion only by default)\n");
+	printf("CL\t  -N INT     total number of input items [%d]\n", N);
+	printf("CL\t  -n INT     initial number of input items [%d]\n", n0);
+	printf("CL\t  -k INT     number of checkpoints [%d]\n", n_cp);
+	printf("CL\n");
 
-	step = (max - n) / m;
-	for (i = 0; i <= m; ++i, n += step) {
-		double t, mem;
-		uint32_t size;
-		t = cputime();
-		size = test_int(n, x0);
-		t = cputime() - t;
-		mem = (peakrss() - m0) / 1024.0 / 1024.0;
-		printf("%d\t%d\t%.3f\t%.3f\t%.4f\t%.4f\n", i, n, t, mem, t * 1e6 / n, mem * 1e6 / size);
+	cp = (udb_checkpoint_t*)calloc(n_cp, sizeof(*cp));
+
+	t0 = udb_cputime();
+	sum = udb_traverse_rng(N, x0);
+	t_keygen = udb_cputime() - t0;
+	printf("TG\t%.3f\t%ld\n", t_keygen, (long)sum); // need to print sum; otherwise the compiler may optimize udb_traverse_rng() out
+
+	udb_measure(0, 0, 0, &cp0);
+	test_int(N, n0, is_del, x0, n_cp, cp);
+
+	for (i = 0; i < n_cp; ++i) {
+		double t, m;
+		t = (cp[i].t - cp0.t - t_keygen * cp[i].n_input / N) / cp[i].n_input * 1e6;
+		m = (cp[i].mem - cp0.mem) / cp[i].table_size;
+		printf("M%c\t%d\t%.3f\t%.3f\t%.4f\t%.2f\n", is_del? 'D' : 'I', cp[i].n_input, cp[i].t - cp0.t, (cp[i].mem - cp0.mem) * 1e-6, t, m);
 	}
+	free(cp);
 	return 0;
 }
--- khashl/test.c
+++ khashl/test.c
@@ -1,28 +1,29 @@
 #include "../common.c"
 #include "khashl.h"
 
-KHASHL_MAP_INIT(, intmap_t, intmap, uint32_t, int, hash_fn, kh_eq_generic)
+KHASHL_MAP_INIT(, intmap_t, intmap, uint32_t, int, udb_hash_fn, kh_eq_generic)
 
-uint32_t test_int(uint32_t n, uint32_t x0)
+void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_cp, udb_checkpoint_t *cp)
 {
-	uint32_t i, x, z = 0;
-	intmap_t *h;
-	h = intmap_init();
-	for (i = 0, x = x0; i < n; ++i) {
-		khint_t k;
-		int absent;
-		x = hash32(x);
-		k = intmap_put(h, get_key(n, x), &absent);
-#ifndef UDB2_TEST_DEL
-		if (absent) kh_val(h, k) = 0;
-		z += ++kh_val(h, k);
-#else
-		if (absent) ++z;
-		else intmap_del(h, k);
-#endif
+	uint32_t step = (N - n0) / (n_cp - 1);
+	uint32_t i, x, n, j;
+	uint64_t z = 0;
+	intmap_t *h = intmap_init();
+	for (j = 0, i = 0, n = n0, x = x0; j < n_cp; ++j, n += step) {
+		for (; i < n; ++i) {
+			khint_t k;
+			int absent;
+			x = udb_hash32(x);
+			k = intmap_put(h, udb_get_key(N, x), &absent);
+			if (is_del) {
+				if (absent) ++z;
+				else intmap_del(h, k);
+			} else {
+				if (absent) kh_val(h, k) = 0;
+				z += ++kh_val(h, k);
+			}
+		}
+		udb_measure(n, kh_size(h), z, &cp[j]);
 	}
-	fprintf(stderr, "# unique keys: %d; checksum: %u; capacity: %u\n", kh_size(h), z, kh_capacity(h));
-	x = kh_size(h);
 	intmap_destroy(h);
-	return x;
 }
