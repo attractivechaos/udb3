b9d146d348f8f19a334b41f11335b909940945dd
Attractive Chaos <attractor@live.co.uk>
2025-03-14 21:30:19 +0800

    use memory pool, necessary for deletions

--- kavl/test.c
+++ kavl/test.c
@@ -1,10 +1,12 @@
 #include "../common.c"
 #include "kavl.h"
+#include "kmempool.h"
 
 typedef struct aux_s {
 	uint32_t key, cnt;
 	KAVL_HEAD(struct aux_s) head;
 } aux_t;
+
 #define aux_cmp(a, b) (((a)->key > (b)->key) - ((a)->key < (b)->key))
 KAVL_INIT(32, aux_t, head, aux_cmp)
 
@@ -14,7 +16,8 @@ void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_c
 	uint32_t i, n, j;
 	uint64_t z = 0, x = x0;
 	aux_t *root = 0, *p, *q;
-	p = (aux_t*)malloc(sizeof(aux_t));
+	void *mp = kmp_init(sizeof(aux_t));
+	p = (aux_t*)kmp_alloc(mp);
 	for (j = 0, i = 0, n = n0; j < n_cp; ++j, n += step) {
 		for (; i < n; ++i) {
 			uint64_t y = udb_splitmix64(&x);
@@ -22,11 +25,11 @@ void test_int(uint32_t N, uint32_t n0, int32_t is_del, uint32_t x0, uint32_t n_c
 			q = kavl_insert(32, &root, p, 0);
 			if (is_del) {
 			} else {
-				if (p == q) p = (aux_t*)malloc(sizeof(aux_t));
+				if (p == q) p = (aux_t*)kmp_alloc(mp);
 				z += ++q->cnt;
 			}
 		}
 		udb_measure(n, kavl_size(head, root), z, &cp[j]);
 	}
-	kavl_free(aux_t, head, root, free);
+	kmp_destroy(mp);
 }
